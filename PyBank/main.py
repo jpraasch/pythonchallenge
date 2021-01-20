0222925b7f5017b60720cb82f02c27400dfd6fb4
Joe R <jpraasch@gmail.com>
2021-01-19 19:22:15 -0600

    PyBank start

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
-print("Hello from PyBank")
+import os
+import csv
+
+csvbank = os.path.join ('..','Resources','budget_data.csv')
+
+
+
+
+
+
+
+
+
+
+
+#Print out Analysis
+print("Financial Analysis")
+print("---------------------------------------------------------------------")
+#total months
+#Total ($)
+#total average change ($0)
+#Greatest Increase in Profits (Month-Year Amount)
+#Greats Decreas in Profits (See above)

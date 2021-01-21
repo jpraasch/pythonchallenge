051026dcc7f88ca1971b7ee7bc1d0feb7aa4b071
Joe R <jpraasch@gmail.com>
2021-01-20 20:57:31 -0600

    Code works, but not right output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,39 +1,53 @@
 import os
 import csv
 
-#Open and Read CSV
+# Open and Read CSV
 csvpoll = os.path.join("Resources", "election_data.csv")
 
-#reading the header row, printing and setting it aside (Need assitance from mutilpe online sources)
+# reading the header row, printing and setting it aside (Need assitance from mutilpe online sources)
 with open(csvpoll) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     next(csv_file)
 
-    #Declaring Variables
-    votecount = []
-    totalvotecount = 0
-    candidateslist = []
-    named_candidate_list = []
-    votepercent = []
+    # Declaring Variables as empty Dictiionaries and lists
+    candidates = {}
+    count = 0
+    votes_cast = 0
+    percentvote = 0
+    mostvotes = 0
+    mostvotenames = ""
 
-    #Starting count
+    # counting of the votes
     for row in csv_reader:
-        #total number of votes
-        totalvotecount = totalvotecount + 1
-        #Set Candidates to a list
-        candidateslist.append(row[2])
-        #Find the unique names (shrawantee github)
+        candidatevote = row[2]
+        count += 1
+        # found if online and on someone github (314cascio), the keys functions being used to compare the names in row two and allocate 
+        # the right canidate
+        if candidatevote in candidates.keys():
+            candidates[candidatevote] =+ 1
+        else:
+            candidates[candidatevote] = 1
+    print("Election Results")
+    print("-------------------------")
+    print("Total Votes: {count}")
+    print("-------------------------")
 
-    for x in set(candidateslist):
-        named_candidate_list.append(x)
-        # v is the total number of votes
-        v = candidateslist.count(x)
-        votecount.append(v)
-        #p is finding the percentage
-        p = (v/totalvotecount)*100
-        votepercent.append(p)
 
-        
+    # Finding the total # votes and casting it to a variable
+    for candidatevote in candidates:
+        votes_cast =+ candidates[candidatevote]
+
+        # finding the percent for each candidate (using 314cascio code to keep code clean)
+        percentvote = (candidates[candidatevote])/(count) * 100
+        print(f"{candidates}: {int(percentvote)} % {votes_cast}")
+
+        if candidates[candidatevote] > mostvotes:
+            mostvotenames = candidates
+            mostvotes = candidates[candidatevote]
+
+    print("-------------------------")
+    print(f"WINNER: {mostvotenames}")
+    print("-------------------------")
 
 
 
